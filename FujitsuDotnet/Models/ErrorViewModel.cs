c5c4538327ff49f5bd36aef446bcdddd094aaf68
Irfan Yahya Abdillah <irfanyahyaa@gmail.com>
2024-06-19 21:33:39 +0700

    initial commit

---
+++ FujitsuDotnet/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace FujitsuDotnet.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
